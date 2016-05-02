d3df438287aedf3bec01b4ca90d9954cd5ee475d
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-05-02 20:15:00 +0300

    trigger build

--- spec.js
+++ spec.js
@@ -2,6 +2,7 @@ describe('Protractor Demo App', function() {
   it('should have prop', function() {
     browser.get('http://localhost:7000');
     var test = element(by.id('test'));
+    
     expect(test.getText()).toEqual('yo'); 
   });
 });
